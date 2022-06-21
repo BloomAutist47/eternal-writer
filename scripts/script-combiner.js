f443b4b31d01cd11d840c326f35ba7a027a8f26f
BloomAutist47 <BloomAutist47@users.noreply.github.com>
2022-06-21 10:06:17 +0800

    updared list and made it tab better. also fixed rerender which creates new page history entries.

--- scripts/script-combiner.js
+++ scripts/script-combiner.js
@@ -1,27 +1,69 @@
 /*jshint esversion: 9 */
 
-const fs = require('fs-extra');
 
-const pathFolder = 'src/template/.eternal/js/components/';
-const pathSave = 'src/template/.eternal/js/component.js';
+import fs from 'fs-extra';
+import path from 'path';
+import {minify} from 'minify';
 
+// Location
+const pathComponent = 'src/template/.eternal/js/components/';
+const pathRender = 'src/template/.eternal/js/renders/';
+const pathSave = 'src/template/.eternal/js/main.min.js';
+
+// Var
 var script = '/*jshint esversion: 9 */\n\n';
 
-fs.readdirSync(pathFolder).forEach(file => {
-  if (file.includes(".js")) {
-    const js = fs.readFileSync(pathFolder + file, 'utf8');
-    script += js.replace("/*jshint esversion: 9 */", "").trim() + "\n\n";
-  }
-});
-
-fs.readdirSync(pathFolder + 'editor/').forEach(file => {
-    if (file.includes(".js")) {
-      const js = fs.readFileSync(pathFolder + 'editor/' + file, 'utf8');
-      script += js.replace("/*jshint esversion: 9 */", "").trim() + "\n\n";
-    }
+let files  = [];
+function ThroughDirectory(Directory) {
+  fs.readdirSync(Directory).forEach(File => {
+      const Absolute = path.join(Directory, File);
+      if (fs.statSync(Absolute).isDirectory()) return ThroughDirectory(Absolute);
+      else return files.push(Absolute);
   });
+}
+
+
+
+// Combine Render
+files  = [];
+ThroughDirectory(pathRender);
+for (const file of files) {
+  const js = fs.readFileSync(file, 'utf8');
+  script += js.replace("/*jshint esversion: 9 */", "").trim() + "\n\n";
+}
+
+
+// Combine Components
+files  = [];
+ThroughDirectory(pathComponent);
+for (const file of files) {
+  const js = fs.readFileSync(file, 'utf8');
+  script += js.replace("/*jshint esversion: 9 */", "").trim() + "\n\n";
+}
 
 
 fs.writeFileSync(pathSave, script, {encoding: 'utf-8'});  
 
-console.log("Done");
+// Minify
+async function minifyFile(pathSave) {
+  const options = {
+    html: {
+        removeAttributeQuotes: false,
+        removeOptionalTags: false,
+    },
+    js: {
+      compress: true,
+      mangle: true,
+    }
+  };
+
+  const data = await minify(pathSave, 'name', options);
+  fs.writeFileSync(pathSave, data, {encoding: 'utf-8'});  
+  console.log("Done");
+}
+
+minifyFile(pathSave);
+
+
+const pathSaveCSS = 'src/template/.eternal/css/style.css';
+minifyFile(pathSaveCSS);
